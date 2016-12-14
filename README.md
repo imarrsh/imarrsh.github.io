cb95c2cc7b2b9f72d5ed21b633782c07006c70eb
Marshall Thompson <marshallthomp@gmail.com>
2016-12-14 17:29:17 -0500

    publish: moving repos around
    generated from commit 78916c129258eb91fc664caeec3b8143b7a2cfa3

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Site
-
-Simple web page showcasing a bit about me and some of my recent work!
--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-const $ = require('jquery');
-require('./tracking'); // require analytics
-
-// DOM ready
-$(function(){
-  const hello = () => 'Coming Soon!!';
-  console.log(hello());
-}())
