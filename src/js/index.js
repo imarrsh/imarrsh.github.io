ce887ff65e5af7b3750373d1eee244a22a0db5d4
Marshall Thompson <marshallthomp@gmail.com>
2016-12-20 10:44:40 -0500

    added fonts and began mobile first nav

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,13 @@ require('./tracking'); // require analytics
 
 // DOM ready
 $(function(){
+
   const hello = () => 'Coming Soon!!';
   console.log(hello());
+
+  // mobile menu toggle
+  $('.mobile-menu').on('click', function(){
+    $('.nav-body').slideToggle();
+  });
+
 }())
