526e81f51f0de5e8054b3a91ac3c995199979b0c
Marshall Thompson <marshallthomp@gmail.com>
2016-12-20 13:00:02 -0500

    rough code in place for menu-bar

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,24 @@ require('./tracking'); // require analytics
 // DOM ready
 $(function(){
 
-  const hello = () => 'Coming Soon!!';
-  console.log(hello());
-
   // mobile menu toggle
-  $('.mobile-menu').on('click', function(){
+  $('.mobile-menu').on('click', () => {
     $('.nav-body').slideToggle();
   });
+
+  //check scroll pos of menu bar and stick it
+  $(window).scroll(function() {
+    const $navBar = $('#nav-bar') // grab nav-bar
+    , heroht = $('#top').height() // calc height of hero
+    , wScroll = $(this).scrollTop(); // calc scoll distance
+
+    if (heroht <= wScroll) {
+      $navBar.addClass('isFixed');
+    } else {
+      $navBar.removeClass('isFixed');
+    }
+
+    console.log(heroht, wScroll);
+  })
 
 }())
