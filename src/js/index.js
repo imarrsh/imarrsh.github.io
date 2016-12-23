55ae08b004fd3aba1318c2b3d49461a3c800d217
Marshall Thompson <marshallthomp@gmail.com>
2016-12-22 21:28:31 -0500

    refining mobile nav and overall styles

--- src/js/index.js
+++ src/js/index.js
@@ -5,23 +5,39 @@ require('./tracking'); // require analytics
 $(function(){
 
   // mobile menu toggle
-  $('.mobile-menu').on('click', () => {
-    $('.nav-body').slideToggle();
+  $('.mobile-menu').on('click', function() {
+    $(this).toggleClass('menu-close');
+    $('.nav-body').toggleClass('open');
   });
 
   //check scroll pos of menu bar and stick it
   $(window).scroll(function() {
     const $navBar = $('#nav-bar') // grab nav-bar
+    , $spacer = $('.nav-bar__spacer') // grab spacer
     , heroht = $('#top').height() // calc height of hero
     , wScroll = $(this).scrollTop(); // calc scoll distance
 
-    $navBar.removeClass('isFixed');
+    // if ($navbar.hasClass('isFixed')) {
+    //   $navBar.removeClass('isFixed');
+    // }
 
     if (heroht <= wScroll) {
       $navBar.addClass('isFixed');
+      $spacer.show();
+    } else {
+      $navBar.removeClass('isFixed');
+      $spacer.hide();
     }
-
-    console.log(heroht, wScroll);
+    
   })
+
+  const getYear = () => {
+    const date = new Date()
+    , year = date.getFullYear();
+
+    return year;
+  }
+
+  $('#year').text(getYear());
 
 }());
