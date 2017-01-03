c35da6c45b3447cb877a8747d1fed8487a3fcef8
Marshall Thompson <marshallthomp@gmail.com>
2017-01-03 10:43:11 -0500

    add outbound link tracking

--- src/js/tracking.js
+++ src/js/tracking.js
@@ -19,3 +19,26 @@ analytics('send', 'pageview', {
         'page': location,
         'title': pageTitle
 });
+
+/**
+* Function that tracks a click on an outbound link in Analytics.
+* This function has been modified to be an event handler that takes the 
+* event object and pulls the URL string from the href attribute 
+* of the currentTarget and uses that URL string as the 
+* event label. Setting the transport method to 'beacon' lets the hit be sent
+* using 'navigator.sendBeacon' in browser that support it.
+*/
+const trackOutboundLink = function(e) {
+  e.preventDefault();
+  const url = e.currentTarget.href;
+
+   ga('send', 'event', 'outbound', 'click', url, {
+     'transport': 'beacon',
+     'hitCallback': function(){document.location = url;}
+   });
+}
+
+// collect outbound links
+const outboundLinks = document.querySelectorAll('a[href^="http"]');
+// console.log(outboundLinks);
+outboundLinks.forEach(link => link.addEventListener('click', trackOutboundLink) );
