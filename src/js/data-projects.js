afb32f394c42566097fa196905b4acef9b6fc6ac
Marshall Thompson <marshallthomp@gmail.com>
2017-01-06 13:33:35 -0500

    switched to template literal for work tiles

---
+++ src/js/data-projects.js
@@ -0,0 +1,50 @@
+// data file for projects - keep logic clean!
+
+const projects = [
+    {
+      "name": "Surf &amp; Paddle",
+      "title" : "Pixel Perfect with Surf & Paddle",
+      "description": "This project wrapped up week 1 of the cohort and presented the challenge of matching a design mock-up right up to the last pixel. The extra credit was to make the site responsive. Features Normalize.css and CSS3 media queries.",
+      "liveLink": "https://imarrsh.github.io/1.4-pixel-perfect/",
+      "codeLink": "https://github.com/imarrsh/1.4-pixel-perfect",
+      "imageUrl" : "images/surf-n-paddle.png" 
+    },{
+      "name": "Powell Peralta",
+      "title": "Advanced CSS with Sass Project",
+      "description": "Week 2 introduced the cohort to Sassy CSS, Bootstrap-Sass and Bower Package Manager. This project builds upon the technology and concepts from week one and features some CSS3 transitions.",
+      "liveLink": "https://imarrsh.github.io/2.2-sass-sb-layout/",
+      "codeLink": "https://github.com/imarrsh/2.2-sass-sb-layout",
+      "imageUrl": "images/powell-peralta.png"
+    },{
+      "name": "GitHub Profile",
+      "title": "Handlebars templating",
+      "description": "Week 4 introduced jQuery, AJAX, Handlebars and more. This project is static re-creation of the repo view on my GitHub account - it pulls live data from the GitHub API and uses Handlebars templates to generate the repository&nbsp;list.",
+      "liveLink": "https://imarrsh.github.io/4.4-github-profile/",
+      "codeLink": "https://github.com/imarrsh/4.4-github-profile",
+      "imageUrl" : "images/github-profile.png"
+    },{
+      "name": "Etsy Recreation",
+      "title": "More Handlebars and JSONP fetching",
+      "description": "Also in week 4, the goal of this project was to utilize live data from Etsy’s database and recreate the search page. This project utilizes Handlebars templates for the search list items and taxonomy paths. Go ahead, try searching for something!",
+      "liveLink": "https://imarrsh.github.io/4.2-etsy-recreation/",
+      "codeLink": "https://github.com/imarrsh/4.2-etsy-recreation",
+      "imageUrl" : "images/etsy-recreation.png"
+    },{
+      "name": "Majestic Thai",
+      "title": "Majestic Thai",
+      "description": "The good stuff from week 7 - an app built with React + Backbone to simulate an ordering system for a fictitious Thai restaurant. Try placing an order and be sure to check out the link to the kitchen view at the bottom!",
+      "liveLink": "https://imarrsh.github.io/7.4-react-majestic-thai/",
+      "codeLink": "https://github.com/imarrsh/7.4-react-majestic-thai",
+      "imageUrl" : "images/majestic-thai.png"
+    },{
+      "name": "Batch Maker",
+      "title": "More Backbone and React!",
+      "description": "This was the last major project before diving head-first into the final project. This app features user authentication, CRUD functionality and utilizes React + Backbone to get it done.",
+      "liveLink": "https://imarrsh.github.io/8.4-batch-maker/",
+      "codeLink": "https://github.com/imarrsh/8.4-batch-maker",
+      "imageUrl" : "images/batch-maker.png"
+    }
+  ];
+
+  module.exports = projects;
+  
